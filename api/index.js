fd97cdbe8681f7abe2dbcb5b5480cec747fcd188
Tawfeeq111 <mohammedtawfeeq945@gmail.com>
2024-09-10 20:52:35 +0530

    render

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import authRouter from "./routes/auth.route.js"
-import userRouter from "./routes/user.route.js"
-import listingRouter from "./routes/listing.route.js"
+import authRouter from "./routes/auth.route.js";
+import userRouter from "./routes/user.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path"
 
 dotenv.config();
 const app = express();
@@ -18,6 +19,9 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log("Error connecting to DB : ", err.message);
 })
 
+
+const __dirname = path.resolve();
+
 app.listen(3000, () => {
     console.log("Listening on port 3000");
 })
@@ -25,6 +29,12 @@ app.listen(3000, () => {
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+})
 
 app.use((err, req, res, next) => {
     console.log(err)
